ae86d1132f045c05c494db83503a344449621729
CrepeGoat <awqatty.b@gmail.com>
2025-12-07 12:27:34 -0700

    did e.g. proof
    https://leanprover.zulipchat.com/#narrow/channel/113489-new-members/topic/prob.20and.20stats.20e.2Eg.2E.20in.20Lean4.20-.20linearity.20of.20expectation/

--- SampleVariance/MomentsOfSums.lean
+++ SampleVariance/MomentsOfSums.lean
@@ -8,33 +8,26 @@ open scoped ENNReal
 -- https://leanprover-community.githb.io/blog/posts/basic-probability-in-mathlib/#:~:text=Measure%20%CE%A9.-,Identically%20distributed,-IdentDistrib%20X%20Y
 -- https://leanprover-community.github.io/blog/posts/basic-probability-in-mathlib/#:~:text=of%20the%20indicator).-,Independence,-Mathlib%20has%20several
 
-noncomputable def moment_of_sum
+noncomputable def sum_random_vars
   {R : Type u_1} [NormedAddCommGroup R] [NormedSpace ℝ R] [CommSemiring R] -- [MeasurableSpace R]
   {Ω : Type u_2} [MeasurableSpace Ω]
-  (P : Measure Ω)
   {n : ℕ}
   (X : (Fin n) → Ω → R)
   -- (hX : (i : Fin n) → Measurable (X i))
-  (k : ℕ)
-  : R := P[fun (ω : Ω) => (∑' i : Fin n, X i ω) ^ k]
-
+  : (Ω → R) := fun (ω : Ω) => (∑ i : Fin n, X i ω)
 
 example
   {Ω : Type u_1} [m : MeasurableSpace Ω]
   {P : Measure Ω} [IsProbabilityMeasure P]
-  {P2 : Measure ℝ}
-  {μ : ℝ}
-  {σ : ℝ≥0}
-  {X : Fin 3 → Ω → ℝ}
-  (hX : (i : Fin 3) → Measurable (X i))
-  (hX2 : (i : Fin 3) → AEStronglyMeasurable (X i) P)
-  (h_indep : iIndepFun X P)
-  : (moment_of_sum P X 1 = ∑' i : Fin 3, moment (X i) 1 P)
+  {n : ℕ}
+  {X : Fin n → Ω → ℝ}
+  -- (hX : (i : Fin n) → Measurable (X i))
+  (hXIntegrable : (i : Fin n) → Integrable (X i) P)
+  : (moment (sum_random_vars X) 1 P = ∑ i : Fin n, moment (X i) 1 P)
   := by
-  unfold moment_of_sum
+  unfold sum_random_vars
   unfold moment
   simp only [pow_one, Pi.pow_apply]
-  rw [integral_tsum hX2]
-  simp
-  -- rw [pow_one]
-  sorry
+  rw [integral_finset_sum]
+  intro i _
+  exact hXIntegrable i
