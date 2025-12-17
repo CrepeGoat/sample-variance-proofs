5f3bb1eb479a3ed200c1ab85aefff36f5a17393b
CrepeGoat <awqatty.b@gmail.com>
2025-12-17 01:38:24 -0700

    code from zulip chat
    https://leanprover.zulipchat.com/#narrow/channel/113489-new-members/topic/stuck.20on.20a.20proof.20on.20probability.20expectations/with/564137993

--- SampleVariance/MomentsOfSums.lean
+++ SampleVariance/MomentsOfSums.lean
@@ -1,4 +1,4 @@
-import Mathlib
+-- import Mathlib
 
 import Mathlib.Analysis.Normed.Group.Basic
 import Mathlib.MeasureTheory.MeasurableSpace.Defs
@@ -7,11 +7,12 @@ import Mathlib.MeasureTheory.Measure.Typeclasses.Probability
 import Mathlib.Probability.Independence.Basic
 import Mathlib.Probability.Moments.Basic
 import Mathlib.MeasureTheory.Function.L1Space.Integrable
+import Mathlib.Data.Finset.Range
 
 import SampleVariance.PowOfSums
 
 -- https://leanprover-community.github.io/blog/posts/basic-probability-in-mathlib/
-open MeasureTheory ProbabilityTheory NNReal
+open Finset MeasureTheory ProbabilityTheory NNReal
 open scoped ENNReal
 
 
@@ -100,7 +101,6 @@ theorem k_moment_sum_expand
     rfl
 
 
-set_option maxHeartbeats 0 in
 theorem k_moment_sum_recursive
   {Ω : Type u_1} [m : MeasurableSpace Ω]
   {μ : Measure Ω} [IsProbabilityMeasure μ]
@@ -138,3 +138,66 @@ theorem k_moment_sum_recursive
       -- rw [Nat.add_one]
       -- rw [integral_mul_const_of_integrable]
       -- rw [hXIndep]
+
+
+variable {Ω : Type*} {mΩ : MeasurableSpace Ω} {P : Measure Ω} [IsFiniteMeasure P]
+    {n k : ℕ} {X : Fin (n + 1) → Ω → ℝ}
+
+theorem MemLp.integrable_pow
+  {f : Ω → ℝ}
+  (h : MemLp f k P)
+  : Integrable (fun x => f x ^ k) P
+  := by
+  obtain rfl | hk := eq_or_ne k 0
+  · simp only [pow_zero, enorm_one, ne_eq, ENNReal.one_ne_top, not_false_eq_true,
+    integrable_const_enorm]
+  rw [← integrable_norm_iff]
+  · simpa [← memLp_one_iff_integrable] using h.norm_rpow (by norm_cast) (by simp)
+  exact h.aestronglyMeasurable.pow k
+
+example (hX : ∀ i, MemLp (X i) k P) (h : iIndepFun X P) :
+    moment (∑ i, X i) k P =
+    ∑ l ∈ range (k + 1),
+      (k.choose l)
+      * moment (X (Fin.last n)) (k - l) P
+      * moment (∑ i : Fin n, X i.castSucc) l P
+  := by
+  have key (l : ℕ) : (fun ω => X (Fin.last n) ω ^ (k - l)) ⟂ᵢ[P] fun ω => (∑ i : Fin n, X i.castSucc ω) ^ l
+    := by
+    apply IndepFun.comp
+    · symm
+      have : (fun ω ↦ ∑ i : Fin n, X i.castSucc ω) = ∑ i ∈ Iio (Fin.last n), X i
+        := by ext; simp only [Fin.Iio_last_eq_map, sum_apply, sum_map, Fin.coe_castSuccEmb]
+      rw [this]
+      apply h.indepFun_finset_sum_of_notMem₀
+      · exact fun i ↦ (hX i).aemeasurable
+      · simp only [Fin.Iio_last_eq_map, mem_map, mem_univ, Fin.coe_castSuccEmb,
+        Fin.castSucc_ne_last, and_false, exists_false, not_false_eq_true]
+    · exact measurable_id.pow_const (k - l)
+    · exact measurable_id.pow_const l
+  unfold moment
+  simp only [Pi.pow_apply, sum_apply]
+  simp_rw [Fin.sum_univ_castSucc, add_pow]
+  rw [integral_finset_sum]
+  · congr with l
+    rw [mul_assoc, ← IndepFun.integral_mul_eq_mul_integral, ← integral_const_mul]
+    · congr with ω
+      simp only [Pi.mul_apply]
+      ring
+    · exact key l
+    · exact (hX _).aestronglyMeasurable.pow (k - l)
+    · convert (aestronglyMeasurable_sum (Finset.univ) (fun (i : Fin n) _ ↦ (hX i.castSucc).aestronglyMeasurable)).pow l
+      simp
+  · intro l hl
+    apply Integrable.mul_const
+    apply IndepFun.integrable_mul
+    · exact (key l).symm
+    · apply MemLp.integrable_pow
+      apply memLp_finset_sum
+      intro i _
+      apply (hX i.castSucc).mono_exponent
+      simp_all only [mem_range, mem_univ, Nat.cast_le]
+      omega
+    · apply MemLp.integrable_pow
+      apply (hX _).mono_exponent
+      simp_all only [mem_range, ENNReal.natCast_sub, tsub_le_iff_right, self_le_add_right]
