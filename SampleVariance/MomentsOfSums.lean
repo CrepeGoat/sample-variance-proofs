c9c87ab5a854bae4d777d04b124842fd9dcb6b57
CrepeGoat <awqatty.b@gmail.com>
2025-12-17 02:12:11 -0700

    reformatted zulip code to match current style

--- SampleVariance/MomentsOfSums.lean
+++ SampleVariance/MomentsOfSums.lean
@@ -28,123 +28,10 @@ noncomputable def isum_rv
   -- (hX : (i : Fin n) → Measurable (X i))
   : (Ω → R) := fun (ω : Ω) => (∑ i : Fin n, X i ω)
 
-
--- -- set_option maxHeartbeats 10000000 in
--- theorem iindepfun_then_indep_isum_drop_last_last
---   {Ω : Type u_1} [MeasurableSpace Ω]
---   {μ : Measure Ω} [IsProbabilityMeasure μ]
---   {n : ℕ}
---   {X : Fin n.succ → Ω → ℝ}
---   (hXIndep : iIndepFun X μ)
---   (m : Fin n)
---   : IndepFun
---     (m.castSucc |> ifun_first_n X |> isum_rv)
---     (n |> Fin.last |> X) μ
---   := by
---   induction m
---   case mk
---   ·
---   sorry
-
-example
-  {Ω : Type u_1} [m : MeasurableSpace Ω]
-  {μ : Measure Ω} [IsProbabilityMeasure μ]
-  {n : ℕ}
-  {X : Fin n → Ω → ℝ}
-  -- (hX : (i : Fin n) → Measurable (X i))
-  (hXIntegrable : (i : Fin n) → Integrable (X i) μ)
-  : (moment (isum_rv X) 1 μ = ∑ i : Fin n, moment (X i) 1 μ)
-  := by
-  unfold isum_rv moment
-  simp only [pow_one, Pi.pow_apply]
-  rw [integral_finset_sum]
-  intro i _
-  exact hXIntegrable i
-
--- https://leanprover-community.github.io/mathlib4_docs/Mathlib/Probability/Independence/Integration.html#ProbabilityTheory.IndepFun.integral_mul_eq_mul_integral
-
-
-example
-  {Ω : Type u_1} [m : MeasurableSpace Ω]
-  {μ : Measure Ω} [IsProbabilityMeasure μ]
-  {n : ℕ}
-  {X : Fin n → Ω → ℝ}
-  -- (hXIntegrable : (i : Fin n) → Integrable (X i) μ)
-  (hXIndep : iIndepFun X μ)
-  : (moment (∏ i : Fin n, X i) 1 μ = ∏ i : Fin n, moment (X i) 1 μ)
-  := by
-  unfold moment
-  simp only [Finset.prod_apply, Pi.pow_apply, pow_one]
-  sorry
-
-
-theorem k_moment_sum_expand
-  {Ω : Type u_1} [m : MeasurableSpace Ω]
-  {μ : Measure Ω} [IsProbabilityMeasure μ]
-  {n : ℕ}
-  (X : Fin n.succ → Ω → ℝ)
-  (k : ℕ)
-  : moment (isum_rv X) k μ
-   = μ[∑ ki : Fin k.succ, (X |> ifun_drop_last |> isum_rv) ^ ki.toNat
-    * (n |> Fin.last |> X) ^ (k - ki) * k.choose ki]
-  := by
-    unfold moment ifun_drop_last isum_rv
-    simp only [Nat.succ_eq_add_one, Pi.pow_apply, Fin.toNat_eq_val,
-      Finset.sum_apply, Pi.mul_apply, Pi.natCast_apply]
-    rewrite [<- MeasureTheory.setIntegral_univ]
-    nth_rewrite 2 [<- MeasureTheory.setIntegral_univ]
-    apply setIntegral_congr_fun
-    · exact MeasurableSet.univ
-    rw [Set.eqOn_univ, funext_iff]
-    intro ω
-    rw [pow_sum_castSucc_eq_sum_add_pow]
-    rfl
-
-
-theorem k_moment_sum_recursive
-  {Ω : Type u_1} [m : MeasurableSpace Ω]
-  {μ : Measure Ω} [IsProbabilityMeasure μ]
-  {n : ℕ}
-  (X : Fin n.succ → Ω → ℝ)
-  (k : ℕ)
-  (hXIntegrable : (i : Fin n.succ) → Integrable (X i) μ)
-  (hXIndep : iIndepFun X μ)
-  -- (hXIdent : (i : Fin n.succ) → (j : Fin n.succ) → IdentDistrib (X i) (X j) μ μ)
-  : moment (isum_rv X) k μ
-    = ∑ ki : Fin k.succ, moment (X |> ifun_drop_last |> isum_rv) ki.toNat μ
-      * moment (n |> Fin.last |> X) (k - ki) μ * (k.choose ki).cast
-  := by
-    rw [k_moment_sum_expand]
-    simp only [Nat.succ_eq_add_one, Fin.toNat_eq_val, Finset.sum_apply,
-      Pi.mul_apply, Pi.pow_apply, Pi.natCast_apply]
-    rw [integral_finset_sum]
-    case hf =>
-      intro i _
-      rw [integrable_mul_const_iff]
-      case hc =>
-        rw [isUnit_iff_ne_zero, ne_eq, Nat.cast_eq_zero, <- ne_eq]
-        apply Nat.choose_ne_zero
-        rw [@Nat.le_iff_lt_add_one]
-        exact i.is_lt
-      sorry
-    sorry
-
-
-
-
-      -- rw [Integrable.const_mul]
-
-      -- have h : Integrable (X |> ifun_drop_last |> isum_rv) ^ ki.toNat * (n |> Fin.last |> X) ^ (k - ki) * k.choose ki μ := sorry
-      -- rw [Nat.add_one]
-      -- rw [integral_mul_const_of_integrable]
-      -- rw [hXIndep]
-
-
-variable {Ω : Type*} {mΩ : MeasurableSpace Ω} {P : Measure Ω} [IsFiniteMeasure P]
-    {n k : ℕ} {X : Fin (n + 1) → Ω → ℝ}
-
 theorem MemLp.integrable_pow
+  {Ω : Type*} {mΩ : MeasurableSpace Ω} {P : Measure Ω} [IsFiniteMeasure P]
   {f : Ω → ℝ}
+  {k : ℕ}
   (h : MemLp f k P)
   : Integrable (fun x => f x ^ k) P
   := by
@@ -155,12 +42,20 @@ theorem MemLp.integrable_pow
   · simpa [← memLp_one_iff_integrable] using h.norm_rpow (by norm_cast) (by simp)
   exact h.aestronglyMeasurable.pow k
 
-example (hX : ∀ i, MemLp (X i) k P) (h : iIndepFun X P) :
-    moment (∑ i, X i) k P =
-    ∑ l ∈ range (k + 1),
-      (k.choose l)
-      * moment (X (Fin.last n)) (k - l) P
-      * moment (∑ i : Fin n, X i.castSucc) l P
+-- https://leanprover.zulipchat.com/#narrow/channel/113489-new-members/topic/stuck.20on.20a.20proof.20on.20probability.20expectations/near/564137993
+theorem k_moment_sum_recursive
+  {Ω : Type u_1} [m : MeasurableSpace Ω]
+  {P : Measure Ω} [IsFiniteMeasure P]
+  {n : ℕ}
+  (X : Fin n.succ → Ω → ℝ)
+  (k : ℕ)
+  (hX : ∀ i, MemLp (X i) k P)
+  (hXIndep : iIndepFun X P)
+  : moment (∑ i, X i) k P
+    = ∑ l ∈ range (k + 1),
+    (k.choose l)
+    * moment (X (Fin.last n)) (k - l) P
+    * moment (∑ i : Fin n, X i.castSucc) l P
   := by
   have key (l : ℕ) : (fun ω => X (Fin.last n) ω ^ (k - l)) ⟂ᵢ[P] fun ω => (∑ i : Fin n, X i.castSucc ω) ^ l
     := by
@@ -169,7 +64,7 @@ example (hX : ∀ i, MemLp (X i) k P) (h : iIndepFun X P) :
       have : (fun ω ↦ ∑ i : Fin n, X i.castSucc ω) = ∑ i ∈ Iio (Fin.last n), X i
         := by ext; simp only [Fin.Iio_last_eq_map, sum_apply, sum_map, Fin.coe_castSuccEmb]
       rw [this]
-      apply h.indepFun_finset_sum_of_notMem₀
+      apply hXIndep.indepFun_finset_sum_of_notMem₀
       · exact fun i ↦ (hX i).aemeasurable
       · simp only [Fin.Iio_last_eq_map, mem_map, mem_univ, Fin.coe_castSuccEmb,
         Fin.castSucc_ne_last, and_false, exists_false, not_false_eq_true]
