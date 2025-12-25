cda9c9a93dee211200c9b24a083577a719a180ae
CrepeGoat <awqatty.b@gmail.com>
2025-12-25 01:00:24 -0500

    proved equivalent expression for biased sample variance

--- SampleVariance/SampleVariance.lean
+++ SampleVariance/SampleVariance.lean
@@ -11,37 +11,33 @@ import Mathlib.Data.Finset.Range
 open Finset MeasureTheory ProbabilityTheory NNReal
 open scoped ENNReal
 
-noncomputable def mean
+noncomputable def smean
   {n : ℕ}
   (X : Fin n → ℝ)
   : ℝ := (∑ i : Fin n, X i) / n
 
-noncomputable def biased_var
+noncomputable def biased_svar
   {n : ℕ}
   (X : Fin n → ℝ)
-  : ℝ := (∑ i : Fin n, ((X i - mean X) ^ 2)) / n
+  : ℝ := (∑ i : Fin n, ((X i - smean X) ^ 2)) / n
 
-theorem biased_var_eq_mean_sq_add_sq_mean
+theorem biased_svar_eq_smean_sq_add_sq_smean
   {n : ℕ}
   (X : Fin (n + 1) → ℝ)
-  : biased_var X = mean (fun i => X i ^ 2) - mean X ^ 2
+  : biased_svar X = smean (fun i => X i ^ 2) - smean X ^ 2
   := by
-  unfold biased_var mean
+  unfold biased_svar smean
   simp only
-  -- simp only [<- Nat.cast_add, Nat.cast_one]
 
   have h : @Nat.cast ℝ Real.instNatCast (n + 1) ≠ 0 := by
     rw [Nat.cast_ne_zero, ne_eq, Nat.add_eq_zero]
     simp only [one_ne_zero, and_false, not_false_eq_true]
 
   rw [div_eq_iff h]
-  rw [sub_mul]
-  rw [div_mul_comm]
+  rw [sub_mul, div_mul_comm]
   nth_rw 2 [(div_eq_one_iff_eq h).mpr (by rfl)]
   rw [one_mul]
-  rw [mul_comm]
-  -- rw [<- sum_const (@HPow.hPow ℝ ℕ ℝ instHPow ((∑ i, X i) / ↑(n + 1)) 2)]
-  -- rw [<- sum_sub_distrib]
+
   conv =>
     lhs
     congr
@@ -49,45 +45,21 @@ theorem biased_var_eq_mean_sq_add_sq_mean
     · intro i
       rw [sub_sq]
   rw [sum_add_distrib, sum_sub_distrib]
-  rw [sum_const]
+  rw [sub_eq_add_neg, sub_eq_add_neg, add_assoc, add_right_inj]
+  rw [sum_const, card_univ, Fintype.card_fin, nsmul_eq_mul, div_pow]
 
-
-
-  -- rw [@div_pow]
-  -- nth_rw 2 [pow_succ]
-  -- rw [div_mul_eq_div_div]
-  -- rw [div_sub_div]
-  -- case hb =>
-  --   rw [@Nat.cast_ne_zero, Nat.add_one]
-  --   simp only [Nat.succ_eq_add_one, ne_eq, Nat.add_eq_zero, one_ne_zero, and_false,
-  --     not_false_eq_true]
-  -- case hd =>
-  --   rw [@Nat.cast_ne_zero, Nat.add_one]
-  --   simp only [Nat.succ_eq_add_one, ne_eq, Nat.add_eq_zero, one_ne_zero, and_false,
-  --     not_false_eq_true]
-  -- rw [div_mul_eq_div_div]
-  -- simp only [Nat.cast_add, Nat.cast_one, pow_one]
-  -- rw [eq_iff_eq_of_div_eq_div]
-
-  sorry
-  -- calc
-  --   (∑ i, (X i - mean X) ^ 2) / n
-  --     = (∑ i, (X i ^ 2 - 2 * X i * mean X + mean X ^ 2)) / n
-  --     := by
-  --     conv =>
-  --       lhs
-  --       congr
-  --       · congr
-  --         · skip
-  --         · intro i
-  --           rw [sub_sq]
-  --       · skip
-  --   _ = (∑ i, (X i ^ 2) - ∑ i, (2 * X i * mean X) + ∑ _ : Fin n, (mean X ^ 2)) / ↑n
-  --     := by
-  --       rw [div_left_inj]
-  --       sorry
-  --   := by sorry
-  -- sorry
-
+  rw [<- sum_mul, <- mul_sum, mul_assoc, mul_div, <- sq]
+  nth_rw 3 [mul_comm]
+  rw [<- eq_sub_iff_add_eq, sub_eq_add_neg, neg_mul_eq_neg_mul, <- two_mul, mul_neg,
+    neg_mul_eq_neg_mul, mul_right_inj' (by simp only [ne_eq, neg_eq_zero, OfNat.ofNat_ne_zero,
+      not_false_eq_true])]
+  rw [div_eq_iff h, mul_comm, <- mul_assoc, <- sq, mul_comm, div_mul_comm]
+  nth_rw 1 [<- one_mul (@HPow.hPow ℝ ℕ ℝ instHPow (∑ i, X i) 2)]
+  by_cases (@HPow.hPow ℝ ℕ ℝ instHPow (∑ i, X i) 2) = 0
+  case pos h2 => rw [h2, mul_zero, mul_zero]
+  case neg h2 =>
+    rw [<- ne_eq] at h2
+    rw [mul_left_inj' h2]
+    simp_all only [ne_eq, OfNat.ofNat_ne_zero, not_false_eq_true, pow_eq_zero_iff, div_self]
 
 -- ProbabilityTheory.variance_eq_sub
